b8acf07bdcad126dde6582c70e09eb4d07c3d797
Walks2000 <walks2000@googlemail.com>
2017-01-05 21:49:51 +0000

    Sided Maps + choosing

--- Module1.vb
+++ Module1.vb
@@ -8,6 +8,14 @@
     Public HalfTime As Boolean = True
     Public KnifeRound As Boolean = True
     Public MapPool(6) As String
+    Public ScrambledMapPool(6) As String
+    Public SideCT As String
+    Public SideT As String
+    Public SideCTPercent As Integer
+    Public SideTPercent As Integer
+    Public Team1Side As String
+    Public Team2Side As String
+
     Public Sub Main()
         Console.Clear()
         RandomWinner = Rand.Next(0, 101)
@@ -22,7 +30,6 @@
         MapPool(4) = "Nuke"
         MapPool(5) = "Overpass"
         MapPool(6) = "Train"
-        Dim ScrambledMapPool(6) As String
         For i = 0 To 6
             Dim RandomMap As Integer
                 RandomMap = Rand.Next(0, 7)
@@ -81,6 +88,7 @@
             Console.WriteLine("That is not valid, please try again now.")
             Overtime = Console.ReadLine
         End While
+        Console.Clear()
         Console.WriteLine("Please enter the name of the first team now.")
         Team1 = Console.ReadLine
         Console.WriteLine("Please enter the name of the second team now.")
@@ -105,9 +113,89 @@
 
         Console.Clear()
 
-        If KnifeRound = True Then
-
+        Console.WriteLine("The map is: {0}", ScrambledMapPool(0))
+        If ScrambledMapPool(0) = "Cache" Then
+            SideCTPercent = 54
+            SideTPercent = 46
         End If
+
+        If ScrambledMapPool(0) = "Dust2" Then
+            SideCTPercent = 49
+            SideTPercent = 51
+        End If
+
+        If ScrambledMapPool(0) = "Cobblestone" Then
+            SideCTPercent = 52
+            SideTPercent = 48
+        End If
+
+        If ScrambledMapPool(0) = "Mirage" Then
+            SideCTPercent = 54
+            SideTPercent = 46
+        End If
+
+        If ScrambledMapPool(0) = "Nuke" Then
+            SideCTPercent = 60
+            SideTPercent = 40
+        End If
+
+        If ScrambledMapPool(0) = "Overpass" Then
+            SideCTPercent = 57
+            SideTPercent = 43
+        End If
+
+        If ScrambledMapPool(0) = "Train" Then
+            SideCTPercent = 56
+            SideTPercent = 44
+        End If
+
+        If KnifeRound = True Then
+            RandomWinner = Rand.Next(0, 101)
+            If RandomWinner >= 50 Then
+                Console.WriteLine("{0} have won the knife round.", Team1)
+                If SideCTPercent > SideTPercent Then
+                    Console.WriteLine("{0} have went with the CT side.", Team1)
+                    SideCT = Team1
+                    SideT = Team2
+                    Team1Side = "CT"
+                    Team2Side = "T"
+                Else
+                    Console.WriteLine("{0} have went with the T side.", Team1)
+                    SideT = Team1
+                    SideCT = Team2
+                    Team2Side = "CT"
+                    Team1Side = "T"
+                End If
+            Else
+                Console.WriteLine("{0} have won the knife round.", Team2)
+                If SideCTPercent > SideTPercent Then
+                    Console.WriteLine("{0} have went with the CT side.", Team2)
+                    SideCT = Team2
+                    SideT = Team1
+                    Team2Side = "CT"
+                    Team1Side = "T"
+                Else
+                    Console.WriteLine("{0} have went with the T side.", Team2)
+                    SideT = Team2
+                    SideCT = Team1
+                    Team1Side = "CT"
+                    Team2Side = "T"
+                End If
+                SideCT = Team2
+                SideT = Team1
+            End If
+        End If
+
+        System.Threading.Thread.Sleep(3000)
+        Dim LO3 As Integer = 3
+        For i = 1 To 3
+            Console.WriteLine("Going live in {0}...", LO3)
+            LO3 = LO3 - 1
+            System.Threading.Thread.Sleep(1000)
+        Next
+
+        Console.Clear()
+
 
         If OvertimeFormat = "1" Then
             'Winning Scores
@@ -156,6 +244,19 @@
                         End If
                     Next
                 End If
+                If HalfTimes.Contains(i) Then
+                    If SideCT = Team1 And SideT = Team2 Then
+                        SideCT = Team2
+                        SideT = Team1
+                        Team1Side = "T"
+                        Team2Side = "CT"
+                    Else
+                        SideCT = Team1
+                        SideT = Team2
+                        Team1Side = "CT"
+                        Team2Side = "T"
+                    End If
+                End If
                 'Begin round + round mechanics
                 RandomWinner = Rand.Next(0, 101)
                 If Team1Score = 45 And Team2Score < 45 Then
@@ -166,14 +267,26 @@
                 End If
                 Console.WriteLine("The round has begun...")
                 System.Threading.Thread.Sleep(Rand.Next(1000, 2001))
-                If RandomWinner > 50 Then
-                    Team1Score = Team1Score + 1
-                    Console.WriteLine("{0} has taken the round.", Team1)
-                    Console.WriteLine("The score is now {0}: {1} - {2}: {3}", Team1, Team1Score, Team2, Team2Score)
+                If RandomWinner >= SideTPercent Then
+                    If SideCT = Team1 Then
+                        Team1Score = Team1Score + 1
+                        Console.WriteLine("{0} has taken the round.", Team1)
+                        Console.WriteLine("The score is now ({0}) {1}: {2} - ({3}) {4}: {5}", Team1Side, Team1, Team1Score, Team2Side, Team2, Team2Score)
+                    Else
+                        Team2Score = Team2Score + 1
+                        Console.WriteLine("{0} has taken the round.", Team2)
+                        Console.WriteLine("The score is now ({0}) {1}: {2} - ({3}) {4}: {5}", Team1Side, Team1, Team1Score, Team2Side, Team2, Team2Score)
+                    End If
                 Else
-                    Team2Score = Team2Score + 1
-                    Console.WriteLine("{0} has taken the round.", Team2)
-                    Console.WriteLine("The score is now {0}: {1} - {2}: {3}", Team1, Team1Score, Team2, Team2Score)
+                    If SideT = Team1 Then
+                        Team1Score = Team1Score + 1
+                        Console.WriteLine("{0} has taken the round.", Team1)
+                        Console.WriteLine("The score is now ({0}) {1}: {2} - ({3}) {4}: {5}", Team1Side, Team1, Team1Score, Team2Side, Team2, Team2Score)
+                    Else
+                        Team2Score = Team2Score + 1
+                        Console.WriteLine("{0} has taken the round.", Team2)
+                        Console.WriteLine("The score is now ({0}) {1}: {2} - ({3}) {4}: {5}", Team1Side, Team1, Team1Score, Team2Side, Team2, Team2Score)
+                    End If
                 End If
             Loop
 
@@ -203,7 +316,7 @@
 
             'Start game
             Dim i As Integer = 0
-            Do Until WinningScores.Contains(Team1Score) Or WinningScores.Contains(Team2Score)
+            Do Until WinningScores.Contains(Team1Score) AndAlso Team2Score < Team1Score - 1 Or WinningScores.Contains(Team2Score) AndAlso Team1Score < Team2Score - 1
                 i = i + 1
                 'Wait between round beginnings and endings
                 If i > 1 Then
@@ -226,6 +339,19 @@
                         End If
                     Next
                 End If
+                If HalfTimes.Contains(i) Then
+                    If SideCT = Team1 And SideT = Team2 Then
+                        SideCT = Team2
+                        SideT = Team1
+                        Team1Side = "T"
+                        Team2Side = "CT"
+                    Else
+                        SideCT = Team1
+                        SideT = Team2
+                        Team1Side = "CT"
+                        Team2Side = "T"
+                    End If
+                End If
                 'Begin round + round mechanics
                 RandomWinner = Rand.Next(0, 101)
                 If Team1Score = 45 And Team2Score < 45 Then
@@ -236,14 +362,14 @@
                 End If
                 Console.WriteLine("The round has begun...")
                 System.Threading.Thread.Sleep(Rand.Next(1000, 2001))
-                If RandomWinner > 50 Then
+                If RandomWinner >= 50 Then
                     Team1Score = Team1Score + 1
                     Console.WriteLine("{0} has taken the round.", Team1)
-                    Console.WriteLine("The score is now {0}: {1} - {2}: {3}", Team1, Team1Score, Team2, Team2Score)
+                    Console.WriteLine("The score is now ({0}) {1}: {2} - ({3}) {4}: {5}", Team1Side, Team1, Team1Score, Team2Side, Team2, Team2Score)
                 Else
                     Team2Score = Team2Score + 1
                     Console.WriteLine("{0} has taken the round.", Team2)
-                    Console.WriteLine("The score is now {0}: {1} - {2}: {3}", Team1, Team1Score, Team2, Team2Score)
+                    Console.WriteLine("The score is now ({0}) {1}: {2} - ({3}) {4}: {5}", Team1Side, Team1, Team1Score, Team2Side, Team2, Team2Score)
                 End If
             Loop
 
@@ -253,19 +379,37 @@
     End Sub
     Sub OvertimeFalse()
         Console.Clear()
-        'Start Rounds
-        For i = 1 To 30
-            'If there is a winner, quit
-            If Team1Score = 16 Or Team2Score = 16 Then
-                DetermWin()
-            End If
+        'Winning Scores
+        Dim WinningScores(10) As Integer
+        WinningScores(0) = "16"
+        For WinningScoresLoop As Integer = 1 To 10 Step 1
+            WinningScores(WinningScoresLoop) = 16 + (3 * WinningScoresLoop)
+        Next
+
+        'Max Rounds // DEPRECATED
+        Dim MaxRounds(10) As Integer
+        MaxRounds(0) = "30"
+        For WinningScoresLoop As Integer = 1 To 10 Step 1
+            MaxRounds(WinningScoresLoop) = 30 + (6 * WinningScoresLoop)
+        Next
+
+        'Half times
+        Dim HalfTimes(10) As Integer
+        HalfTimes(0) = "16"
+        For WinningScoresLoop As Integer = 1 To 10 Step 1
+            HalfTimes(WinningScoresLoop) = 30 + (3 * WinningScoresLoop)
+        Next
+        'Start game
+        Dim i As Integer = 0
+        Do Until WinningScores.Contains(Team1Score) AndAlso Team2Score < Team1Score - 1 Or WinningScores.Contains(Team2Score) AndAlso Team1Score < Team2Score - 1
+            i = i + 1
             'Wait between round beginnings and endings
             If i > 1 Then
                 Console.WriteLine("")
                 System.Threading.Thread.Sleep(Rand.Next(500, 2001))
             End If
             'Halftime logic
-            If i = 16 Then
+            If HalfTimes.Contains(i) And HalfTime = True Then
                 Dim Timer As Integer = 5
                 System.Threading.Thread.Sleep(Rand.Next(500, 1501))
                 Console.Clear()
@@ -280,20 +424,37 @@
                     End If
                 Next
             End If
+            If HalfTimes.Contains(i) Then
+                If SideCT = Team1 And SideT = Team2 Then
+                    SideCT = Team2
+                    SideT = Team1
+                    Team1Side = "T"
+                    Team2Side = "CT"
+                Else
+                    SideCT = Team1
+                    SideT = Team2
+                    Team1Side = "CT"
+                    Team2Side = "T"
+                End If
+            End If
             'Begin round + round mechanics
             RandomWinner = Rand.Next(0, 101)
+            If Team1Score = 15 And Team2Score = 15 Then
+                DetermWin()
+            End If
             Console.WriteLine("The round has begun...")
             System.Threading.Thread.Sleep(Rand.Next(1000, 2001))
-            If RandomWinner > 50 Then
+            If RandomWinner >= 50 Then
                 Team1Score = Team1Score + 1
                 Console.WriteLine("{0} has taken the round.", Team1)
-                Console.WriteLine("The score is now {0}: {1} - {2}: {3}", Team1, Team1Score, Team2, Team2Score)
+                Console.WriteLine("The score is now ({0}) {1}: {2} - ({3}) {4}: {5}", Team1Side, Team1, Team1Score, Team2Side, Team2, Team2Score)
             Else
                 Team2Score = Team2Score + 1
                 Console.WriteLine("{0} has taken the round.", Team2)
-                Console.WriteLine("The score is now {0}: {1} - {2}: {3}", Team1, Team1Score, Team2, Team2Score)
+                Console.WriteLine("The score is now ({0}) {1}: {2} - ({3}) {4}: {5}", Team1Side, Team1, Team1Score, Team2Side, Team2, Team2Score)
             End If
-        Next
+        Loop
+
         System.Threading.Thread.Sleep(Rand.Next(500, 1501))
         DetermWin()
     End Sub
