afe78001c988502b0971d8578ab75664f1559158
Walks2000 <walks2000@googlemail.com>
2017-01-05 21:10:39 +0000

    Delete useless code + test

--- Module1.vb
+++ Module1.vb
@@ -5,7 +5,8 @@
     Public Team2Score As Integer
     Public Rand As New Random
     Public RandomWinner As Integer = Rand.Next(0, 101)
-    Public HalfTimeEnabled As Boolean = True
+    Public HalfTime As Boolean = True
+    Public KnifeRound As Boolean = True
     Public MapPool(6) As String
     Public Sub Main()
         Console.Clear()
@@ -45,20 +46,30 @@
     End Sub
     Sub Configure()
         Console.Clear()
-        Console.WriteLine("1. Half Time Enabled: {0}", HalfTimeEnabled)
+        Console.WriteLine("1. Half Time: {0}", HalfTime)
+        Console.WriteLine("2. Knife Round: {0}", KnifeRound)
         Console.WriteLine("9. Home")
 
         Dim UserChoice As String = Console.ReadLine
         Select Case UserChoice
             Case 1
-                If HalfTimeEnabled = True Then
-                    HalfTimeEnabled = False
+                If HalfTime = True Then
+                    HalfTime = False
                 Else
-                    HalfTimeEnabled = True
+                    HalfTime = True
+                End If
+                Configure()
+            Case 2
+                If KnifeRound = True Then
+                    KnifeRound = False
+                Else
+                    KnifeRound = True
                 End If
                 Configure()
             Case 9
                 Main()
+            Case Else
+                Configure()
         End Select
     End Sub
     Sub GameSetup()
@@ -94,6 +105,10 @@
 
         Console.Clear()
 
+        If KnifeRound = True Then
+
+        End If
+
         If OvertimeFormat = "1" Then
             'Winning Scores
             Dim WinningScores(10) As Integer
@@ -126,7 +141,7 @@
                     System.Threading.Thread.Sleep(Rand.Next(500, 2001))
                 End If
                 'Halftime logic
-                If HalfTimes.Contains(i) And HalfTimeEnabled = True Then
+                If HalfTimes.Contains(i) And HalfTime = True Then
                     Dim Timer As Integer = 5
                     System.Threading.Thread.Sleep(Rand.Next(500, 1501))
                     Console.Clear()
@@ -196,7 +211,7 @@
                     System.Threading.Thread.Sleep(Rand.Next(500, 2001))
                 End If
                 'Halftime logic
-                If HalfTimes.Contains(i) And HalfTimeEnabled = True Then
+                If HalfTimes.Contains(i) And HalfTime = True Then
                     Dim Timer As Integer = 5
                     System.Threading.Thread.Sleep(Rand.Next(500, 1501))
                     Console.Clear()
@@ -235,135 +250,9 @@
             System.Threading.Thread.Sleep(Rand.Next(500, 1501))
             DetermWin()
         End If
-
-
-        'If OvertimeChance > 75 Then
-        '    If OvertimeFormat = "1" Then
-        '        Dim OvertimeWinsChance As Integer = Rand.Next(0, 101)
-        '        Dim OvertimeMaxRounds = 19
-        '        Dim OvertimeMinRounds = 15
-        '        If OvertimeWinsChance <= 80 Then
-        '            OvertimeMaxRounds = 19
-        '            OvertimeMinRounds = 15
-        '        End If
-        '        If OvertimeWinsChance > 80 And OvertimeWinsChance <= 90 Then
-        '            OvertimeMaxRounds = 22
-        '            OvertimeMinRounds = 18
-        '        End If
-        '        If OvertimeWinsChance > 90 And OvertimeWinsChance <= 95 Then
-        '            OvertimeMaxRounds = 25
-        '            OvertimeMinRounds = 21
-        '        End If
-        '        If OvertimeWinsChance > 95 And OvertimeWinsChance <= 100 Then
-        '            OvertimeMaxRounds = 28
-        '            OvertimeMinRounds = 24
-        '        End If
-        '        While Team1Score <> OvertimeMaxRounds Or Team2Score <> OvertimeMaxRounds
-        '            Team1Score = Rand.Next(OvertimeMinRounds, OvertimeMaxRounds + 1)
-        '            Team2Score = Rand.Next(OvertimeMinRounds, OvertimeMaxRounds + 1)
-        '        End While
-        '        If Team1Score = OvertimeMaxRounds And Team2Score = OvertimeMaxRounds Then
-        '            If RandomWinner > 50 Then
-        '                Team1Score = Rand.Next(OvertimeMinRounds, OvertimeMaxRounds - 1)
-        '                Team2Score = OvertimeMaxRounds
-        '            Else
-        '                Team2Score = Rand.Next(OvertimeMinRounds, OvertimeMaxRounds - 1)
-        '                Team1Score = OvertimeMaxRounds
-        '            End If
-        '        End If
-        '        If Team1Score = OvertimeMaxRounds And Team2Score = OvertimeMaxRounds - 1 Then
-        '            Team2Score = OvertimeMaxRounds - 2
-        '        End If
-        '        If Team1Score = OvertimeMaxRounds - 1 And Team2Score = OvertimeMaxRounds Then
-        '            Team1Score = OvertimeMaxRounds - 2
-        '        End If
-        '    Else
-        '        Dim OvertimeWinsChance As Integer = Rand.Next(0, 101)
-        '        Dim OvertimeMaxRounds = 21
-        '        Dim OvertimeMinRounds = 15
-        '        If OvertimeWinsChance <= 80 Then
-        '            OvertimeMaxRounds = 21
-        '            OvertimeMinRounds = 15
-        '        End If
-        '        If OvertimeWinsChance > 80 And OvertimeWinsChance <= 90 Then
-        '            OvertimeMaxRounds = 26
-        '            OvertimeMinRounds = 20
-        '        End If
-        '        If OvertimeWinsChance > 90 And OvertimeWinsChance <= 95 Then
-        '            OvertimeMaxRounds = 31
-        '            OvertimeMinRounds = 25
-        '        End If
-        '        If OvertimeWinsChance > 95 And OvertimeWinsChance <= 100 Then
-        '            OvertimeMaxRounds = 36
-        '            OvertimeMinRounds = 30
-        '        End If
-        '        While Team1Score <> OvertimeMaxRounds Or Team2Score <> OvertimeMaxRounds
-        '            Team1Score = Rand.Next(OvertimeMinRounds, OvertimeMaxRounds + 1)
-        '            Team2Score = Rand.Next(OvertimeMinRounds, OvertimeMaxRounds + 1)
-        '        End While
-        '        If Team1Score = OvertimeMaxRounds And Team2Score = OvertimeMaxRounds Then
-        '            If RandomWinner > 50 Then
-        '                Team1Score = Rand.Next(OvertimeMinRounds, OvertimeMaxRounds - 1)
-        '                Team2Score = OvertimeMaxRounds
-        '            Else
-        '                Team2Score = Rand.Next(OvertimeMinRounds, OvertimeMaxRounds - 1)
-        '                Team1Score = OvertimeMaxRounds
-        '            End If
-        '        End If
-        '        If Team1Score = OvertimeMaxRounds And Team2Score = OvertimeMaxRounds - 1 Then
-        '            Team2Score = OvertimeMaxRounds - 2
-        '        End If
-        '        If Team1Score = OvertimeMaxRounds - 1 And Team2Score = OvertimeMaxRounds Then
-        '            Team1Score = OvertimeMaxRounds - 2
-        '        End If
-        '    End If
-        'End If
-        'Console.ReadLine()
     End Sub
     Sub OvertimeFalse()
         Console.Clear()
-        'While Team1Score <> 16 Or Team2Score <> 16
-        '    Team1Score = Rand.Next(0, 17)
-        '    Team2Score = Rand.Next(0, 17)
-        '    If Team1Score = 15 And Team2Score = 15 Then
-        '        Exit While
-        '    End If
-        'End While
-        'If Team1Score = 16 And Team2Score = 16 Then
-        '    If RandomWinner > 50 Then
-        '        Team1Score = Rand.Next(0, 15)
-        '    Else
-        '        Team2Score = Rand.Next(0, 15)
-        '    End If
-        'End If
-        'If Team1Score = 16 And Team2Score = 15 Then
-        '    Team2Score = 14
-        'End If
-        'If Team1Score = 15 And Team2Score = 16 Then
-        '    Team1Score = 14
-        'End If
-        'Dim Winner As String
-        'If Team1Score > Team2Score Then
-        '    Winner = Team1
-        'Else
-        '    Winner = Team2
-        'End If
-        'If Team1Score = Team2Score Then
-        '    Winner = "Draw"
-        'End If
-        'If Winner <> "Draw" Then
-        '    Console.WriteLine("The winner is {0}.", Winner)
-        'Else
-        '    Console.WriteLine("The game ended as a draw.")
-        'End If
-        'If Winner = Team1 Then
-        '    Console.WriteLine("The score ended as {0} - {1}.", Team1Score, Team2Score)
-        'Else
-        '    Console.WriteLine("The score ended as {0} - {1}.", Team2Score, Team1Score)
-        'End If
-        'Console.ReadLine()
-
-
         'Start Rounds
         For i = 1 To 30
             'If there is a winner, quit
